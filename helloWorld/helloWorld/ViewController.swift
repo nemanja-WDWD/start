6ca6b9c9883b28026d711717ffb4a6fd736c77b3
nemanja-WDWD <nemanja@wedowebdesign.rs>
2016-12-18 22:13:20 +0100

    Add files via upload

---
+++ helloWorld/helloWorld/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  helloWorld
+//
+//  Created by Mac on 12/15/16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var background: UIImageView!
+    override func viewDidLoad() {
+        @IBOutlet weak var title: UIImageView!
+        @IBOutlet weak var title: UIImageView!
+        super.viewDidLoad()
+        @IBOutlet weak var background: UIImageView!
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
